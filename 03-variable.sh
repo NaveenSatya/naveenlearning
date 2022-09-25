eeda1d5b061b587b478cc55ffa2d7f9c31b79a5d
Naveensatya <naganaveen.satya@gmail.com>
2022-09-25 09:59:48 +0530

    learning how to use the variable concept adding another variable and accessing it

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,8 +3,11 @@
 A=10
 echo Naveen = $A
 
+b=20
+echo satya = $b
+
 ## Command Subs
-#NO_OF_USERS=$(who |wc -l)
+NO_OF_USERS=$(who |wc -l)
 echo Number of Users = $NO_OF_USERS
 
 ## Date with command substitution
